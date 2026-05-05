524bb887d9a044fa0c557c6b8f3ac4f4bc29433e
Josue Gutierrez <jagr.developer@gmail.com>
2026-05-05 00:06:58 -0600

    Fix AdMob Kotlin metadata incompatibility and consent callback

---
+++ app/src/main/java/com/jagr/fridamusic/data/ads/AdConfig.kt
@@ -0,0 +1,7 @@
+package com.jagr.fridamusic.data.ads
+
+object AdConfig {
+    // Official Google test ad unit IDs
+    const val BANNER_AD_UNIT_ID = "ca-app-pub-3940256099942544/9214589741"
+    const val NATIVE_AD_UNIT_ID = "ca-app-pub-3940256099942544/2247696110"
+}
