613140ae74ad43af20b062e5cadb5e71711ff8c4
Josue Gutierrez <jagr.developer@gmail.com>
2026-05-04 23:59:20 -0600

    Integrate AdMob foundation with consent and ad managers

---
+++ app/src/main/java/com/jagr/fridamusic/data/ads/GoogleMobileAdsConsentManager.kt
@@ -0,0 +1,38 @@
+package com.jagr.fridamusic.data.ads
+
+import android.app.Activity
+import android.content.Context
+import com.google.android.ump.ConsentInformation
+import com.google.android.ump.ConsentRequestParameters
+import com.google.android.ump.UserMessagingPlatform
+
+class GoogleMobileAdsConsentManager(context: Context) {
+    private val consentInformation: ConsentInformation =
+        UserMessagingPlatform.getConsentInformation(context)
+
+    val canRequestAds: Boolean
+        get() = consentInformation.canRequestAds()
+
+    fun requestConsent(
+        activity: Activity,
+        onCompleted: () -> Unit
+    ) {
+        val params = ConsentRequestParameters.Builder().build()
+        consentInformation.requestConsentInfoUpdate(
+            activity,
+            params,
+            {
+                UserMessagingPlatform.loadAndShowConsentFormIfRequired(activity) {
+                    onCompleted()
+                }
+            },
+            {
+                onCompleted()
+            }
+        )
+
+        if (consentInformation.canRequestAds()) {
+            onCompleted()
+        }
+    }
+}
